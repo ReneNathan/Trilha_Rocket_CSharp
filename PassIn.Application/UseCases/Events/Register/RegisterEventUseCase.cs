176f20ada012008501bde3f98d818771693653de
ReneNathan <rene.nathan@cajamar.sp.gov.br>
2024-04-02 11:40:54 -0300

    Primeiro commit com o início da aula 1

---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,12 @@
+﻿using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventUseCase
+    {
+        public void execute(RequestEventJson request)
+        {
+
+        }
+    }
+}
