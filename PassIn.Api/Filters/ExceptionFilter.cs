3506e81050e975e8e2d68c4983d67fb23981f2c9
ReneNathan <rene.nathan@cajamar.sp.gov.br>
2024-04-03 13:37:41 -0300

    Aula 2 parte_1

---
+++ PassIn.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using System.Net;
+
+namespace PassIn.Api.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var result = context.Exception is PassInException; 
+
+            if (result)
+            {
+                HandleProjectException();
+            }
+            else
+            {
+                ThrowUnknowError(context);
+            }
+        }
+
+        private void HandleProjectException(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+            context.Result = new ObjectResult(new ResponseErrorJson(context.Exception.Message));
+        }
+
+        private void ThrowUnknowError(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = new ObjectResult(new ResponseErrorJson("Unknown error"));
+        }
+    }
+}
